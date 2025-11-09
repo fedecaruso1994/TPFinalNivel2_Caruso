40d3c1d46c1b108cb9c65e0a4f909ea280371431
Fede Caruso <federicocaruso.19@gmail.com>
2025-11-09 11:04:50 -0300

    creacion de clase acceso a datos y metodo listar en articulos

---
+++ presentacion/ArticuloNegocio.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace presentacion
+{
+    class ArticuloNegocio
+    {
+        public List<Articulo> Listar()
+        {
+            List<Articulo> listaArticulos = new List<Articulo>();
+            AccesoDatos accesoDatos = new AccesoDatos();
+            try
+            {
+                accesoDatos.SetearConsulta("SELECT Codigo, Nombre, Descripcion, Precio FROM articulos");
+                accesoDatos.EjecutarLectura();
+
+                while (accesoDatos.Lector.Read())
+                {
+                    Articulo aux = new Articulo();
+                    aux.Codigo = (string)accesoDatos.Lector["Codigo"];
+                    aux.Nombre = (string)accesoDatos.Lector["Nombre"];
+                    aux.Descripcion = (string)accesoDatos.Lector["Descripcion"];
+                    aux.Precio = (double)accesoDatos.Lector["Precio"];
+
+                    listaArticulos.Add(aux);
+                }
+
+
+            return listaArticulos;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
+    }
+}
